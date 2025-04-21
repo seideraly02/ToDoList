f8b24ea6412e6fcd2fa8e3a197b64c8085814a35
Almas Abikenov <almas@Almass-MacBook-Air.local>
2025-04-21 19:22:55 +0500

    sayat chert

--- myscript.js
+++ myscript.js
@@ -1,75 +1,26 @@
-"use strict"
+"use strict";
 
-const reviewCard = document.querySelectorAll(".review-card");
-const previousBtn = document.querySelector (".previous-btn");
-const nextBtn = document.querySelector(".next-btn");
+const track = document.querySelector('#carouselTrack');
+const slides = Array.from(track.children);
 
+let currentIndex = 0;
+let slideWidth = slides[0].getBoundingClientRect().width + 32;
+const visibleSlides = 4;
 
-let count = 0;
+function updateCarousel() {
+  const maxIndex = slides.length - visibleSlides;
 
-nextBtn.addEventListener("click", () => {
-    count++;
-    carousel();
-});
+  track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
 
-previousBtn.addEventListener("click", () => {
-    count--; 
-    carousel();
-});
-
-function carousel() {
-
-    if (count === reviewCard.length) {
-        count = 0;
-    }
-    if (count < 0) {
-        count = reviewCard.length - 1;
-    }
-
-    reviewCard.forEach((card) => {
-        let cardWidth = card.offsetWidth;
-
-        card.style.transform = `translateX(${ -(count * cardWidth)}px`
-    })
-
+  currentIndex++;
+  if (currentIndex > maxIndex) {
+    currentIndex = 0;
+  }
 }
 
+setInterval(updateCarousel, 1000);
 
-// phone navigation 
-
-
-const navigation = document.querySelector(".navigation");
-const menuBtn = document.querySelector(".menu-btn");
-
-menuBtn.addEventListener("click", () => {
-    navigation.classList.toggle("navigation-on");
+window.addEventListener('resize', () => {
+  slideWidth = slides[0].getBoundingClientRect().width + 32;
+  updateCarousel();
 });
-
-window.addEventListener("scroll", () => {
-    if (navigation.classList.contains("navigation-on")) {
-        navigation.classList.remove("navigation-on");
-    }
-})
-const track = document.querySelector('.carousel-track');
-    const slides = Array.from(track.children);
-    const nextButton = document.querySelector('.carousel-btn.next');
-    const prevButton = document.querySelector('.carousel-btn.prev');
-    let currentSlide = 0;
-
-    function updateSlidePosition() {
-        const slideWidth = slides[0].getBoundingClientRect().width;
-        track.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
-    }
-
-    nextButton.addEventListener('click', () => {
-        currentSlide = (currentSlide + 1) % slides.length;
-        updateSlidePosition();
-    });
-
-    prevButton.addEventListener('click', () => {
-        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-        updateSlidePosition();
-    });
-
-    window.addEventListener('resize', updateSlidePosition);
-    updateSlidePosition();
