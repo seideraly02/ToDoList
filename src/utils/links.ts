cf2ddeab700c7bd8c9137aa44f0f3c657a315e9b
Sayat <s.seideraly@enpf.kz>
2026-07-15 18:54:51 +0500

    ашч

--- src/utils/links.ts
+++
@@ -1,6 +0,0 @@
-import { CONTACTS } from '@/config/site'
-
-export const createWhatsappLink = (message: string): string => {
-  const encodedMessage = encodeURIComponent(message)
-  return `https://wa.me/${CONTACTS.whatsappPhone}?text=${encodedMessage}`
-}
